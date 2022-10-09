7292356955d02b4ffa2c9cdda5b38f827a2899e7
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-09 16:19:02 +0200

    water bottle struct, water_menu slice

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+
+type water struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Origin string `json:"origin"`
+	Type   string `json:"type"`
+	Price  int    `json:"price"`
+	Amount int    `json:"amount"`
+}
+
+var water_menu = []water{
+	{Id: 0, Name: "Giga Chad Water", Origin: "Fiji", Type: "mineral", Price: 7, Amount: 10},
+	{Id: 1, Name: "Big Chungus Drink", Origin: "Japan", Type: "sparkling", Price: 20, Amount: 5},
+}
+
 func main() {
 
 }
